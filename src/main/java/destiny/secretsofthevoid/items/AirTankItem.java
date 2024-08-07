a8e8b7aabbf8fa422beb4d5417c79453a172405d
SGUDestiny <panormov2006@gmail.com>
2024-08-08 02:20:00 +0400

    Particles now work

--- src/main/java/destiny/secretsofthevoid/items/AirTankItem.java
+++ src/main/java/destiny/secretsofthevoid/items/AirTankItem.java
@@ -74,9 +74,9 @@ public class AirTankItem extends ArmorItem implements IAirTank {
         if(!stack.getTag().isEmpty()) {
             MutableComponent oxygen = Component.translatable("tooltip.secretsofthevoid.tank_stored_oxygen").withStyle(ChatFormatting.BLUE);
 
-            oxygen.append(Component.literal(getStoredOxygen(stack) + "").withStyle(ChatFormatting.BLUE));
+            oxygen.append(Component.literal(Math.round(getStoredOxygen(stack)) + "").withStyle(ChatFormatting.BLUE));
             oxygen.append(Component.literal(" / ")).withStyle(ChatFormatting.BLUE);
-            oxygen.append(Component.literal("" + getMaxOxygen(stack)).withStyle(ChatFormatting.BLUE));
+            oxygen.append(Component.literal("" + Math.round(getMaxOxygen(stack))).withStyle(ChatFormatting.BLUE));
 
             components.add(oxygen);
         }
