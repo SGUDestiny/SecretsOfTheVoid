417cf629c64287ac9ef3ab2388dead2599bf02d1
mistersecret312 <sergiigamer7@gmail.com>
2024-10-26 14:34:49 +0300

    Mixin.

--- src/main/java/destiny/mixin/CaveBookScreenMixin.java
+++ src/main/java/destiny/mixin/CaveBookScreenMixin.java
@@ -5,11 +5,12 @@ import destiny.secretsofthevoid.SecretsOfTheVoid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CaveBookScreen.class)
 public class CaveBookScreenMixin {
-    @Inject(method = "getBookFileDirectory()IIFV", at = @At("HEAD"))
-    public static String getBookFileDirectory() {
-        return SecretsOfTheVoid.MODID + ":books/";
+    @Inject(method = "getBookFileDirectory()Ljava/lang/String;", at = @At("HEAD"))
+    private static void getBookFileDirectory(CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue(SecretsOfTheVoid.MODID + ":books/");
     }
 }
